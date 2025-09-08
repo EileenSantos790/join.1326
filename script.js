3e2e95bd4a64d3aa844750a0ac7338ff836813ae
Julian Herzfeld <herzfeldjulian@hotmail.de>
2025-09-08 17:45:33 +0200

    script.js added with toggle checkbox function for signup.hmtl, signup.html checkbox adjusted, checked icon added, buttons.css for checkbox in signup.html adjusted, style.css d-none class added

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleCheckbox(checkId, uncheckId, buttonId) {
+    let checked = document.getElementById(checkId);
+    let unchecked = document.getElementById(uncheckId);
+    let buttonStatus = document.getElementById(buttonId);
+    checked.classList.toggle('d-none');
+    unchecked.classList.toggle('d-none');
+    buttonStatus.toggleAttribute('disabled');
+}
