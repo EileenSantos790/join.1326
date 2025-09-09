4f1646c0363c3890cf1f5bc6b304daf1781261f3
Julian Herzfeld <herzfeldjulian@hotmail.de>
2025-09-09 16:10:56 +0200

    index.html visibility icons added and existing lock icon adapted, togglePasswordVisibility function added to show password, signup.hmtl visibility icons added and existing lock icons adapted,

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**
+ * Check the box and activate the button. 
+ */
 function toggleCheckbox(checkId, uncheckId, buttonId) {
     let checked = document.getElementById(checkId);
     let unchecked = document.getElementById(uncheckId);
@@ -5,4 +8,20 @@ function toggleCheckbox(checkId, uncheckId, buttonId) {
     checked.classList.toggle('d-none');
     unchecked.classList.toggle('d-none');
     buttonStatus.toggleAttribute('disabled');
+}
+
+/**
+ * Switches the password and icon between visible and hidden. 
+ */
+function togglePasswordVisibility(inputId, iconOffId, iconOnId) {
+    let input = document.getElementById(inputId);
+    let iconOff = document.getElementById(iconOffId);
+    let iconOn = document.getElementById(iconOnId);
+    iconOff.classList.toggle('d-none');
+    iconOn.classList.toggle('d-none');
+    if(input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
 }
