2a3fd57e7571c5dec136b791afbc617fd48832b7
Julian Herzfeld <herzfeldjulian@hotmail.de>
2025-09-23 05:42:18 +0200

    addTask.html :  input border on focus added, category dropdown with function added, function to close the drop-down menus by clicking outside added, buttons.js renamed to addTask.js

--- js/addTask.js
+++ js/addTask.js
@@ -1,4 +1,4 @@
-let testContactArray = ['Anton A','Bernd B','Clara C','Dori D'];
+let testContactArray = ['Anton A', 'Bernd B', 'Clara C', 'Dori D'];
 let selectedContactsAddTask = [];
 let contactSearchArray = [];
 
@@ -10,7 +10,7 @@ function activatePriorityButton(buttonId, buttonClass, buttonIconOff, buttonIcon
     setPriorityButtonIconToDefault();
     document.getElementById(buttonIconOn).classList.remove('d-none');
     document.getElementById(buttonIconOff).classList.add('d-none');
-    
+
     let currentBtn = document.getElementById(buttonId);
     currentBtn.classList.add(buttonClass);
 }
@@ -65,7 +65,7 @@ function saveClickedContact(containerId) {
     let contactContent = document.getElementById(containerId).textContent;
     let index = selectedContactsAddTask.indexOf(contactContent);
 
-    if(selectedContactsAddTask.includes(contactContent)) {
+    if (selectedContactsAddTask.includes(contactContent)) {
         selectedContactsAddTask.splice(index, 1);
     } else {
         selectedContactsAddTask.push(contactContent);
@@ -92,7 +92,7 @@ function searchContactsForTask() {
     let inputRef = document.getElementById('addTaskSearchInput');
     let searchInput = inputRef.value;
 
-    if(searchInput.length > 0) {
+    if (searchInput.length > 0) {
         contactSearchArray = testContactArray.filter(contact => contact.toLowerCase().includes(searchInput.toLowerCase()));
         renderContactsInAddTask(contactSearchArray);
     } else {
@@ -102,4 +102,63 @@ function searchContactsForTask() {
 
 /**
  * The filter function is not yet working properly because the render function must be called elsewhere. (when opening the addTask page)
- *  */ 
+ *  */
+
+
+function toggleDropdownCategory() {
+    document.getElementById('categoryDropdownContent').classList.toggle('d-none');
+    document.getElementById('dropdownDownIcon').classList.toggle('d-none');
+    document.getElementById('dropdownUpIcon').classList.toggle('d-none');
+    document.getElementById('addTaskCategoryDropdownContent').classList.toggle('boxShadow');
+}
+
+function selectedCategory(option) {
+    let header = document.getElementById('categoryDropdownHeader');
+    header.textContent = option.textContent;
+    header.dataset.value = option.dataset.value;
+    toggleDropdownCategory();
+}
+
+
+/**
+ * close the assigned to and category dropdown when click outside.
+ */
+document.onclick = function (click) {
+    closeAssignedToClickOutside(click);
+    closeCategoryClickOutside(click);
+}
+
+
+function closeAssignedToClickOutside(click) {
+    const dropdownAssignedTo = document.getElementById('addTaskAssignedToDropdownContent');
+
+    if (dropdownAssignedTo && !dropdownAssignedTo.contains(click.target)) {
+        document.getElementById('addTaskDropdownAssignedTo').classList.remove('d-none');
+        document.getElementById('addTaskDropdownSearchContent').classList.add('d-none');
+        document.getElementById('addTaskAddedContactIcons').classList.remove('d-none');
+    }
+}
+
+
+function closeCategoryClickOutside(click) {
+    const dropdownCategory = document.getElementById('addTaskCategoryDropdownContent');
+
+    if (dropdownCategory && !dropdownCategory.contains(click.target)) {
+        document.getElementById('categoryDropdownContent').classList.add('d-none');
+        document.getElementById('dropdownDownIcon').classList.remove('d-none');
+        document.getElementById('dropdownUpIcon').classList.add('d-none');
+        document.getElementById('addTaskCategoryDropdownContent').classList.remove('boxShadow');
+    }
+}
+
+
+function removeFocusBorderCheckInputValue(containerId, inputId, errorId) {
+    document.getElementById(containerId).classList.remove('inputBorderColorFocus');
+    let input = document.getElementById(inputId);
+    let errorMessage = document.getElementById(errorId);
+    if (!input.value) {
+        document.getElementById(containerId).classList.add('inputErrorBorder');
+        errorMessage.textContent = "This field is required";
+
+    }
+}
